ed269e4b408f65472d165ae9475b2fe3fb87d88a
Heva <he.wu@linecorp.com>
2019-07-29 11:52:14 +0900

    Add basic project struct

---
+++ TestVisionCoreML/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TestVisionCoreML
+//
+//  Created by ST21235 on 2019/07/29.
+//  Copyright © 2019 He Wu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
