8a3c02b2c560e0fa8e2aa8945a66c6d0ff9d5097
Heva <he.wu@linecorp.com>
2019-07-29 12:39:44 +0900

    TakePhoto: add basic imageView & button structure

--- TestVisionCoreML/TakePhotoViewController.swift
+++ TestVisionCoreML/TakePhotoViewController.swift
@@ -2,19 +2,45 @@
 //  TakePhotoViewController.swift
 //  TestVisionCoreML
 //
-//  Created by ST21235 on 2019/07/29.
+//  Created by He Wu on 2019/07/29.
 //  Copyright © 2019 He Wu. All rights reserved.
 //
 
 import UIKit
 
-class TakePhotoViewController: UIViewController {
-
+final class TakePhotoViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBAction func takePhotoAction(_ sender: UIButton) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            presentPhotoPicker(sourceType: .photoLibrary)
+            return
+        }
+        presentPhotoPicker(sourceType: .camera)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // core ml model?
     }
 
-
+    private func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true, completion: nil)
+    }
 }
 
+extension TakePhotoViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: - Handling Image Picker Selection
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageView.image = image
+        // showClassifications(for: image)
+    }
+}
